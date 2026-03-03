dc3978c1d1c01c2bd23c42c6e77f2864d6282db7
roshni <roshniadvanifc@gmail.com>
2026-03-03 17:28:24 +0500

    Add files via upload

---
+++ PORTFOLIO/SCRIPT.js
@@ -0,0 +1,8 @@
+// Mobile Menu Toggle
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    nav}
+)
